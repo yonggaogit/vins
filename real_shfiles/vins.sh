165c8e89345ca4fe8d75d7c23375e2c8b233257f
gaoyong <gaoyong_chn@163.com>
2023-11-06 10:36:58 +0800

    backend commit

--- real_shfiles/vins.sh
+++ real_shfiles/vins.sh
@@ -1,7 +1,5 @@
 ag_n=$1
 
-source devel/setup.bash;
-sudo chmod 777 /dev/ttyACM0 & sleep 3;
 roslaunch realsense2_camera rs_camera.launch & sleep 10;
 roslaunch mavros px4.launch & sleep 10;
 roslaunch vins covins_real.launch ag_n:=${ag_n};
--- simulate_shfiles/vins.sh
+++ simulate_shfiles/vins.sh
@@ -1,4 +1,3 @@
 ag_n=$1
 
-source devel/setup.bash;
 roslaunch vins covins0.launch ag_n:=${ag_n};
